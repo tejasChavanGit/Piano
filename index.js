ca9320b5ce0104b04fae9af83b2399bcdd944370
tejasChavanGit <156054120+tejasChavanGit@users.noreply.github.com>
2024-07-30 19:22:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const pianokeys=document.querySelectorAll('.key')
+
+function playSound(newUrl){
+    new Audio(newUrl).play()
+}
+pianokeys.forEach((pianokey,i)=>{
+    const newUrl='24/key'+(i+1)+'.mp3'
+    pianokey.addEventListener('click',()=> playSound(newUrl));
+});
